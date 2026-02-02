4706ecb11afc5e8868ed9ba383ba34f43cbe94cc
pathompornDevjr <pathomporn.wongsuwan@gmail.com>
2026-02-02 14:03:10 +0700

    fix: connectionStr

--- config/pg.js
+++ config/pg.js
@@ -4,7 +4,7 @@ const { Pool } = pg;
 const pool = new Pool({
   connectionString:
     "postgresql://neondb_owner:npg_3RDxrGkq0YVj@ep-solitary-wildflower-a1n6nqdw-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-  ssl: { rejectUnauthorized: false },
+  // ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
